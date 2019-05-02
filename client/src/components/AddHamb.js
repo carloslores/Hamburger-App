06ae2f6b23eb5722d60766e4eab70dcc27784d37
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos-2.local>
2019-05-03 01:10:44 +0200

    implement modal

---
+++ client/src/components/AddHamb.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import Apiservice from "../service/apiservice"
+import Modal from "react-modal"
+
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        width: '50%',
+        height: "50%"
+    }
+}
+
+Modal.setAppElement('#root')
+
+class AddHamb extends Component{
+
+
+render(){
+
+return(
+    
+)
+
+
+}
+
+
+
+
+}
--- server/routes/hamburgers.js
+++ server/routes/hamburgers.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-// const Router       = require('router')
- 
-// const router = Router()
-//const hamburguer = require("../hamburgers.json")
+
+
+const uploader = require('../configs/cloudinary-setup');
 const Hamburger = require("../models/Hamburger")
 
 
@@ -25,6 +24,20 @@ router.get("/hamburgers/:id", (req, res)=>{
     Hamburger.findById(req.params.id)
     .then(data=>res.json(data))
     .catch(err=>console.log(err))
+
+})
+
+router.post("/posthamburger", uploader.single("hamburger"),(req, res)=>{
+
+    const {secure_url} = req.file;
+    const newHamburger = new Hamburger({
+        image: secure_url
+    })
+    newHamburger.save()
+    .then(data=>res.status(200).json(data))
+    .catch(err=>console.log(err))
+
+
 
 })
 
